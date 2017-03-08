534cb00f9ab5c8db269f4c3efd23baff8595ad47
pvanloo <pva@velleman.be>
2017-03-08 11:25:46 +0100

    Add sample code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # KA04
 KA04 sample code
+
+Place the "EtherCard" folder into the "LIBRARIES" folder of your ARDUINO install before trying to compile the ka04_sample_code.ino file.
---
+++ ka04_sample_code/ka04_sample_code.ino
@@ -0,0 +1,61 @@
+
+#include <EtherCard.h>
+
+// ethernet interface mac address, must be unique on the LAN
+static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+
+byte Ethernet::buffer[500];
+BufferFiller bfill;
+
+void setup () {
+  Serial.begin(57600);
+  Serial.println("\n[Starting the Arduino Server]");
+
+  Serial.print("MAC: ");
+  for (byte i = 0; i < 6; ++i) {
+    Serial.print(mymac[i], HEX);
+    if (i < 5)
+      Serial.print(':');
+  }
+  Serial.println();
+  
+  if (ether.begin(sizeof Ethernet::buffer, mymac) == 0) 
+    Serial.println( "Failed to access Ethernet controller");
+
+  Serial.println("Setting up DHCP (this can take a while...)");
+  if (!ether.dhcpSetup())
+    Serial.println( "DHCP failed");
+  
+  ether.printIp("IP: ", ether.myip);
+  ether.printIp("Netmask: ", ether.netmask);
+  ether.printIp("GW IP: ", ether.gwip);
+  ether.printIp("DNS IP: ", ether.dnsip);
+  ether.printIp("Surf to this IP to view the Arduino Server: ", ether.myip);
+}
+
+
+static word homePage() {
+  long t = millis() / 1000;
+  word h = t / 3600;
+  byte m = (t / 60) % 60;
+  byte s = t % 60;
+  bfill = ether.tcpOffset();
+  bfill.emit_p(PSTR(
+    "HTTP/1.0 200 OK\r\n"
+    "Content-Type: text/html\r\n"
+    "Pragma: no-cache\r\n"
+    "\r\n"
+    "<meta http-equiv='refresh' content='1'/>"
+    "<title>Arduino Server -> Active Time</title>" 
+    "<h1>$D$D:$D$D:$D$D</h1>"),
+      h/10, h%10, m/10, m%10, s/10, s%10);
+  return bfill.position();
+}
+
+void loop () {
+  word len = ether.packetReceive();
+  word pos = ether.packetLoop(len);
+  
+  if (pos)  // check if valid tcp data is received
+    ether.httpServerReply(homePage()); // send web page data
+}
